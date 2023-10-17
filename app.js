e0dcc56f4bb7ec5b55df7e6fcf61402a19c400dd
Usama Rahman <rahmanusama390@gmail.com>
2023-10-17 19:53:15 +0600

    w

---
+++ app.js
@@ -0,0 +1,22 @@
+const RANDOM_QOUTE_API_URL = "https://api.quotable.io/random"
+
+const qouteElement = document.getElementById('qouteDispaly');
+const qouteInput = document.getElementById('qouteInput');
+
+function getRandomQoute(){
+    return  fetch(RANDOM_QOUTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
+}
+
+async function getNextQoute (){
+    const qoute  = await getRandomQoute()
+    qouteElement.innerHTML = qoute;
+    
+
+    qouteInput.value = null;
+}
+
+getNextQoute()
+
+// 
